a56c162de195ee6d02809f7f8a4cbb68e14983ed
a9872a9872 <a9872a9872@gmail.com>
2021-03-04 20:58:22 +0800

    add bootstrap and beautify the view

--- index.php
+++ index.php
@@ -23,19 +23,21 @@ if (!empty($_POST['url'])) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>URL Shortener</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
-<body>
-<h1>URL Shortener</h1>
-<form action="./index.php" method="POST">
-    <label>url:
-        <input type="text" name="url">
-    </label>
-    <input type="submit" name="Submit" value="送出">
-</form>
-<?php
-if (isset($new_url)) {
-    echo $new_url;
-}
-?>
+<body class="bg-secondary text-white">
+<div class="container">
+    <h1 class="text-center mt-5">URL Shortener</h1>
+    <form class="input-group mb-3" action="./index.php" method="POST">
+        <input class="form-control" type="text" name="url" placeholder="input the url here">
+        <input class="input-group-text bg-primary" type="submit" name="Submit" value="送出">
+    </form>
+    <?php
+    if (isset($new_url)) {
+        echo '<div class="alert alert-success" role="alert">' . $new_url . '</div>';
+    }
+    ?>
+</div>
 </body>
 </html>
